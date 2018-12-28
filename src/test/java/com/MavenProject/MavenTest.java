49042dce2c56ab6981a015d3346643754cb558b9
sidiquimk <masoodsdq1@gmail.com>
2018-12-27 22:25:06 -0500

    Jenkins clommit two

--- src/test/java/com/MavenProject/MavenTest.java
+++ src/test/java/com/MavenProject/MavenTest.java
@@ -7,6 +7,15 @@ public class MavenTest {
 	@Test
 	public void hello(){
 		System.out.println("this is my first maven test");
-	
 }
+	
+	@Test
+	public void hello1(){
+		System.out.println("this is my first maven test");
+	}
+		
+		@Test
+		public void hello2(){
+			System.out.println("this is my first maven test");
+		}
 }
